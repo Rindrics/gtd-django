e711c45b5b3f76a007ee0528f546e973b29957cf
Akira Hayashi <smxshxishxad@me.com>
2023-03-21 21:03:45 +0900

    :cop: Set sensitive parameters from environmental variables

--- gtd_django/settings.py
+++ gtd_django/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pby#2tx1q&y49rcrl$=w5y(j0!av-l+q!4l(*3vh9s@cdz^e+9'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,11 +80,11 @@ WSGI_APPLICATION = 'gtd_django.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
+        'NAME': env(DATABASE_NAME),
+        'USER': env(DATABASE_USER),
+        'PASSWORD': env(DATABASE_PASSWORD),
+        'HOST': env(DATABASE_HOST),
+        'PORT': env(DATABASE_PORT),
     }
 }
 
