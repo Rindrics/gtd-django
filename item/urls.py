77d2ce1cb22d96210fb7530b2c523070a5801f74
Akira Hayashi <smxshxishxad@me.com>
2023-03-27 03:33:46 +0900

    :sparkles: Enable 'update' and 'delete' of items

--- item/urls.py
+++ item/urls.py
@@ -6,4 +6,6 @@ app_name = 'item'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('capture/', views.capture, name='capture'),
+    path('update/<int:item_id>/', views.update_item, name='update_item'),
+    path('delete/<int:item_id>/', views.delete_item, name='delete_item'),
 ]
--- item/views.py
+++ item/views.py
@@ -1,5 +1,5 @@
 from django.views import generic
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Item
 from .forms import ItemForm
 
@@ -19,3 +19,23 @@ def capture(request):
     items = Item.objects.order_by('-created_at')
     context = {'items': items, 'form': form}
     return render(request, 'capture.html', context)
+
+def update_item(request, item_id):
+    item = get_object_or_404(Item, pk=item_id)
+    if request.method == 'POST':
+        form = ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('item:capture')
+    else:
+        form = ItemForm(instance=item)
+    context = {'form': form}
+    return render(request, 'update_item.html', context)
+
+def delete_item(request, item_id):
+    item = get_object_or_404(Item, pk=item_id)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('item:capture')
+    context = {'item': item}
+    return render(request, 'delete_item.html', context)
